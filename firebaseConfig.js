6af356cd6d3cff78f4378105420f512ff5104164
Ritik Raghuwanshi <ritik.raghuwanshi244@gmail.com>
2023-12-21 15:20:07 +0530

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDygNbd2dgdzbmLODJn4YaOM9GKYBwqZQY",
+  authDomain: "authentication-35060.firebaseapp.com",
+  projectId: "authentication-35060",
+  storageBucket: "authentication-35060.appspot.com",
+  messagingSenderId: "965303673313",
+  appId: "1:965303673313:web:38fa7c72ad859824d3b2a0",
+  measurementId: "G-1S570MQ156"
+};
+
+export default firebaseConfig;
---
+++ src/pages/signin.js
@@ -0,0 +1,156 @@
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import auth from '../../lib/firebase';
+import { useRouter } from 'next/router';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      // Redirect to the home page after successful sign-in
+      router.push('/');
+    } catch (error) {
+      console.error('Sign-in error:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSignUpClick = () => {
+    // Navigate to the sign-up page
+    router.push('/signup');
+  };
+
+  return (
+    <div className="main-container">
+      <div className="signin-container">
+        <h2>Welcome Back!</h2>
+        <form className="signin-form">
+          <label className="form-label">
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-input"
+            />
+          </label>
+          <label className="form-label">
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-input"
+            />
+          </label>
+          <button type="button" onClick={handleSignIn} className="signin-button">
+            {loading ? <div className="loader"></div> : 'Sign In'}
+          </button>
+        </form>
+        <p className="signup-option">
+          Don't have an account? <span onClick={handleSignUpClick}>Sign Up</span>
+        </p>
+      </div>
+
+      <style jsx>{`
+        .main-container {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 90vh;
+            }
+    
+            .signin-container {
+              width: 400px;
+              max-width: 800px;
+              padding: 20px;
+              border: 2px solid #ddd;
+              border-radius: 8px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              background-color: skyblue;
+            }
+    
+            h2 {
+              font-size: 40px;
+              text-align: center;
+              color: black;
+            }
+    
+            .signin-form {
+              display: flex;
+              flex-direction: column;
+              gap: 16px;
+            }
+    
+            .form-label {
+              font-size: 17px;
+              font-weight: 700;
+              display: flex;
+              flex-direction: column;
+              color: gray;
+            }
+    
+            .form-input {
+              padding: 8px;
+              font-size: 16px;
+              border: 1px solid #ccc;
+              border-radius: 4px;
+            }
+    
+            .signin-button {
+              background-color: #3498db;
+              color: #fff;
+              padding: 10px;
+              font-size: 16px;
+              border: none;
+              border-radius: 4px;
+              cursor: pointer;
+            }
+    
+            .signin-button:hover {
+              background-color: #2980b9;
+            }
+    
+            .signup-option {
+              text-align: center;
+              color:white;
+            }
+            
+            span{
+                cursor: pointer;
+                color: #3498db;
+            }
+            .signup-option span:hover {
+              text-decoration: underline;
+    
+            }
+
+        .loader {
+          border: 4px solid #f3f3f3;
+          border-top: 4px solid #3498db;
+          border-radius: 50%;
+          width: 20px;
+          height: 20px;
+          animation: spin 0.8s linear infinite;
+          margin: 0 auto;
+        }
+
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
